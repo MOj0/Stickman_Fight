b8af76d0a57598641c5bb32db2d6a01749c81426
MOj0 <matija.ojo@gmail.com>
2022-01-07 00:40:57 +0100

    Added smooth diffuse

--- shaders.js
+++ shaders.js
@@ -1,102 +1,121 @@
-  const vertex = /*glsl*/`#version 300 es
+const vertex = /*glsl*/`#version 300 es
 
-  uniform mat4 uProjection;
-  uniform mat4 uViewModel;
+uniform mat4 uProjection;
+uniform mat4 uViewModel;
 
-  uniform mat4 uBones[42];
+uniform mat4 uBones[42];
 
-  uniform float uAmbient;
-  uniform float uDiffuse;
-  uniform float uSpecular;
+uniform vec3 uLightPosition;
+uniform vec3 uLightAttenuation;
 
-  uniform float uShininess;
-  uniform vec3 uLightPosition;
-  uniform vec3 uLightColor;
-  uniform vec3 uLightAttenuation;
+out vec3 vEye;
+out vec3 vLight;
+out vec3 vNormal;
+out vec2 vTexCoord;
+out float vAttenuation;
 
-  layout (location = 0) in vec3 aPosition;
-  layout (location = 1) in vec3 aNormal;
-  layout (location = 2) in vec2 aTexCoord;
+layout (location = 0) in vec3 aPosition;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoord;
 
-  layout (location = 3) in highp vec2 aSIndices;
-  layout (location = 4) in highp vec2 aSWeights;
+layout (location = 3) in highp vec2 aSIndices;
+layout (location = 4) in highp vec2 aSWeights;
 
-  out vec3 vLight;
-  out vec2 vTexCoord;
+mat4 boneTransform()
+{
+  mat4 ret;
+  // Weight normalization factor
+  float normfac = 1.0 / (aSWeights.x + aSWeights.y);
+  // Weight1 * Bone1 + Weight2 * Bone2
+  ret = normfac * aSWeights.y * uBones[int(aSIndices.y)]
+      + normfac * aSWeights.x * uBones[int(aSIndices.x)];
+  return ret;
+}
+void main()
+{
+  mat4 bt = (length(aSWeights) > 0.5) ? 
+    boneTransform() 
+    : 
+    mat4(
+        1., 0., 0., 0.,
+        0., 1., 0., 0.,
+        0., 0., 1., 0.,
+        0., 0., 0., 1.
+    );
+  vec3 vertexPosition = (uViewModel * bt * vec4(aPosition, 1)).xyz; // Bone matrix multiplication is here!
+  vec3 lightPosition = (uViewModel * vec4(uLightPosition, 1)).xyz;
 
-  mat4 boneTransform()
+  vEye = -vertexPosition;
+  vLight = lightPosition - vertexPosition;
+  vNormal = (uViewModel * vec4(aNormal, 0)).xyz;
+  vTexCoord = aTexCoord;
+
+  float d = distance(vertexPosition, lightPosition);
+  vAttenuation = 1.0 / dot(uLightAttenuation, vec3(1, d, d * d));
+
+  // TODO: Implement outlining
+  // vec3 normal = normalize(aNormal) * 0.01; // 0.01 - Outline size TODO: Set as uniform
+  // vec3 pos = vertexPosition + normal;
+  // gl_Position = uProjection * vec4(pos, 1);
+
+  gl_Position = uProjection * vec4(vertexPosition, 1);
+}
+`;
+
+const fragment = /*glsl*/`#version 300 es
+precision highp float;
+
+uniform highp sampler2D uTexture;
+
+uniform vec3 uLightColor;
+
+uniform float uAmbient;
+uniform float uDiffuse;
+uniform float uSpecular;
+uniform float uShininess;
+
+uniform vec4 uColor;
+
+in vec3 vEye;
+in vec3 vLight;
+in vec3 vNormal;
+in vec2 vTexCoord;
+in float vAttenuation;
+
+out vec4 oColor;
+
+
+void main()
+{
+  vec3 N = normalize(vNormal);
+  vec3 L = normalize(vLight);
+  vec3 E = normalize(vEye);
+  vec3 R = normalize(reflect(-L, N));
+
+  float lambert = max(0.0, dot(L, N));
+  float phong = pow(max(0.0, dot(E, R)), uShininess);
+
+  float ambient = uAmbient;
+
+  // Diffuse smooth calc
+  float diffuse = dot(vNormal, vLight);
+  float delta = fwidth(diffuse) * 5.0;
+  float diffuseSmooth = smoothstep(0.0, delta, diffuse);
+
+  float specular = uSpecular * phong;
+
+
+  vec3 lightVec = (ambient + diffuseSmooth + specular) * vAttenuation * uLightColor;
+  vec4 light = vec4(lightVec, 1);
+  if(light[0] <= 0.2 && light[1] <= 0.2 && light[2] <= 0.2)
   {
-    mat4 ret;
-
-    // Weight normalization factor
-    float normfac = 1.0 / (aSWeights.x + aSWeights.y);
-
-    // Weight1 * Bone1 + Weight2 * Bone2
-    ret = normfac * aSWeights.y * uBones[int(aSIndices.y)]
-        + normfac * aSWeights.x * uBones[int(aSIndices.x)];
-
-    return ret;
+    light = vec4(0.5, 0.5, 0.5, 1);
   }
 
-  void main()
-  {
-    mat4 bt = (length(aSWeights) > 0.5) ? 
-      boneTransform() 
-      : 
-      mat4(
-          1., 0., 0., 0.,
-          0., 1., 0., 0.,
-          0., 0., 1., 0.,
-          0., 0., 0., 1.
-      );
+  oColor = (uColor == vec4(0, 0, 0, 0) ? texture(uTexture, vTexCoord) : uColor) * light;
+}
+`;
 
-    vec3 vertexPosition = (uViewModel * bt * vec4(aPosition, 1)).xyz; // Bone matrix multiplication is here!
-    vec3 lightPosition = (uViewModel * vec4(uLightPosition, 1)).xyz;
-    float d = distance(vertexPosition, lightPosition);
-    float attenuation = 1.0 / dot(uLightAttenuation, vec3(1, d, d * d));
-
-    vec3 N = (uViewModel * vec4(aNormal, 0)).xyz;
-    vec3 L = normalize(lightPosition - vertexPosition);
-    vec3 E = normalize(-vertexPosition);
-    vec3 R = normalize(reflect(-L, N));
-
-    float lambert = max(0.0, dot(L, N));
-    float phong = pow(max(0.0, dot(E, R)), uShininess);
-
-    float ambient = uAmbient;
-    float diffuse = uDiffuse * lambert;
-    float specular = uSpecular * phong;
-
-    vLight = ((ambient + diffuse + specular) * attenuation) * uLightColor;
-    vTexCoord = aTexCoord;
-
-    gl_Position = uProjection * vec4(vertexPosition, 1);
-  }
-  `;
-
-  const fragment = /*glsl*/`#version 300 es
-
-  precision mediump float;
-
-  uniform mediump sampler2D uTexture;
-  uniform vec4 uColor;
-
-  in vec2 vTexCoord;
-  in vec3 vLight;
-
-  out vec4 oColor;
-
-  void main() {
-    vec4 light = vec4(vLight, 1);
-    if(light[0] <= 0.2 && light[1] <= 0.2 && light[2] <= 0.2) // TODO: Improve?
-    {
-      light = vec4(0.5, 0.5, 0.5, 1);
-    }
-
-    oColor = (uColor == vec4(0, 0, 0, 0) ? texture(uTexture, vTexCoord) : uColor) * light;
-  }
-  `;
-
-  export const shaders = {
-      shader: { vertex, fragment }
-  };
+export const shaders = {
+    shader: { vertex, fragment }
+};
